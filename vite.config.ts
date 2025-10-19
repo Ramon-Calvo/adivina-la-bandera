ad3891c5e07087cd896d1565f50c0c646b5ca14e
Ramon Calvo <ramoncalvo6@gmail.com>
2025-10-19 23:16:37 +0200

    Fix: Configure base URL for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
+﻿import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/adivina-la-bandera/',
   server: {
     port: 3000,
     open: true
