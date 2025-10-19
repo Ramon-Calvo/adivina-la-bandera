1f42afa1f33311489da8b55d33e1b72a1646608d
Ramon Calvo <ramoncalvo6@gmail.com>
2025-10-19 23:20:26 +0200

    Deploy: Update game with corrected base URL configuration

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import './style.css'
-import { FlagGame } from './game'
-
-// Esperar a que el DOM esté completamente cargado
-window.addEventListener('load', () => {
-  const game = new FlagGame()
-  game.init()
-})
